0a7ac84420e6494c6182daa5c185e852647cbd5b
FZY173 <2283442882@qq.com>
2019-09-19 14:26:05 +0800

    2019.9.19

---
+++ PhysicalLaboratory/Assets/Script/drag.cs
@@ -0,0 +1,63 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using System;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class drag : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public GameObject personMainCamera;
+    public GameObject farmar;
+    bool isMove = true;
+    bool isShow = false;
+    Vector3 distance ;
+   
+    void IBeginDragHandler.OnBeginDrag(PointerEventData eventData)
+    {
+        print("start drag");
+    }
+
+    void IDragHandler.OnDrag(PointerEventData eventData)
+    {
+        transform.position = eventData.position;//eventData就是屏幕坐标下的鼠标位置
+            RaycastHit hit = new RaycastHit();
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Physics.Raycast(ray, out hit);
+             distance = hit.point - new Vector3(0, 0, 0);
+        // Debug.Log("distance.x" + distance.x);
+        // Debug.Log("distance.y" + distance.y);
+        isShow = true;
+    }
+
+    void IEndDragHandler.OnEndDrag(PointerEventData eventData)
+    {
+        print("end drag");
+        GameObject.Find("juanchi").transform.position = new Vector3(766.0f, 305.0f, 0.0f);
+        GameObject.Find("fama").transform.position = new Vector3(760.0f, 197.5f, 0.0f);
+        
+    }
+
+    void Start() {
+        distance.x = -2;
+    }
+    void Update()
+    {
+        personMainCamera.GetComponent<FirstPersonController>().enabled = isMove;
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            isMove = !isMove;
+        }
+        if (distance.x > -4f && distance.x < -1f && distance.y > -4f && distance.y < -1f )
+        {
+            if (isShow) {
+                Instantiate(farmar);
+                farmar.transform.position = new Vector3(0.88f, -0.42f, 0.799f);
+            }
+            isShow = false;
+
+        }
+
+    }
+}
+
---
+++ PhysicalLaboratory/Assets/Script/packUp.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;//注意这个不能少
+//using UnityEditor.Sprites ;
+public class packUp : MonoBehaviour
+{
+    //public GameObject Gmenue;
+    public GameObject packUpButton;
+    public GameObject prompts;
+    public Text ddescriptions;
+    int isshow = 0;
+    void Start()
+    {
+        prompts.transform.position =new Vector3(-110,197.7f,0);
+        packUpButton.SetActive(true);
+        ddescriptions.text = "展开";
+
+    }
+
+    void Update()
+    {
+        if (isshow ==1) {
+            ddescriptions.text = "收起";
+            Vector3 distance = prompts.transform.position - new Vector3(-110, 67.7f,0);
+            if (distance.x <= 218) {
+                prompts.transform.position += prompts.transform.right * 100 * Time.deltaTime;
+            }
+        }
+        if (isshow ==2) {
+            ddescriptions.text = "展开";
+            Vector3 distance = prompts.transform.position - new Vector3(-110, 67.7f, 0);
+            if (distance.x >= 0f)
+            {
+                prompts.transform.position -= prompts.transform.right * 100 * Time.deltaTime;
+            }
+           
+        }
+        if (isshow == 3) {
+            isshow = 1;
+        }
+    }
+
+    public void showPeomptsClick() {
+        isshow++;
+    }
+}
+
